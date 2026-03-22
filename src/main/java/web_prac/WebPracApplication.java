647b012503704484ef41ca0e381096248c2434f0
gl-anechka <gl_anechka@mail.ru>
2026-03-23 00:15:09 +0300

    initial commit

---
+++ src/main/java/web_prac/WebPracApplication.java
@@ -0,0 +1,13 @@
+package web_prac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebPracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebPracApplication.class, args);
+	}
+
+}
---
+++ src/test/java/web_prac/WebPracApplicationTests.java
@@ -0,0 +1,13 @@
+package web_prac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebPracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
