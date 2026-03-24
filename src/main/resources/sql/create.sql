f2beb55e4802b714dcdff2d3f4a59b11f80e31c7
gl-anechka <gl_anechka@mail.ru>
2026-03-24 22:08:47 +0300

    classes for tables done

--- src/main/resources/sql/create.sql
+++ src/main/resources/sql/create.sql
@@ -1,12 +1,3 @@
--- перечислимый тип для определения поставщика/потребителя
-CREATE TYPE partner_type_enum AS ENUM ('PROVIDER', 'CONSUMER', 'BOTH');
-
--- перечислимый тип для определения единицы измерения
-CREATE TYPE unit_enum AS ENUM ('kg', 'pcs', 'l');
-
--- статус (степень свежести) товара по времени нахождения на складе
-CREATE TYPE store_status_enum AS ENUM ('OK', 'NEAR_EXPIRY', 'SPOILED');
-
 -- тип товара
 CREATE TABLE product_type (
     id_type SERIAL PRIMARY KEY,
@@ -19,7 +10,7 @@ CREATE TABLE product (
     id_product SERIAL PRIMARY KEY,
     title VARCHAR(300) NOT NULL,
     id_type INTEGER NOT NULL REFERENCES product_type(id_type),
-    unit unit_enum NOT NULL,
+    unit VARCHAR(10) NOT NULL CHECK (unit IN ('KG', 'PCS', 'L')),
     kg_per_unit REAL NOT NULL CHECK (kg_per_unit >= 0)
 );
 
@@ -31,7 +22,8 @@ CREATE TABLE partner (
     address TEXT,
     phone VARCHAR(12),
     email VARCHAR(100),
-    type partner_type_enum NOT NULL DEFAULT 'BOTH'
+    type VARCHAR(20) NOT NULL DEFAULT 'BOTH'
+        CHECK (type IN ('PROVIDER', 'CONSUMER', 'BOTH'))
 );
 
 -- поставка: 1 запись таблицы - поставка одного товара
@@ -44,7 +36,7 @@ CREATE TABLE supply (
 );
 
 -- получение: 1 строка - 1 заказ товара
--- атрибут completed показывает статус заказа: выполнен - 1, обрабатывается - 0
+-- атрибут completed показывает статус заказа: выполнен - true, обрабатывается - false
 CREATE TABLE reception (
     id_reception SERIAL PRIMARY KEY,
     id_consumer INTEGER NOT NULL REFERENCES partner(id_partner),
@@ -64,6 +56,7 @@ CREATE TABLE place (
     CONSTRAINT unique_place UNIQUE (room_num, shelf_num)
 );
 
+-- складская запись
 CREATE TABLE storehouse (
     id_storehouse SERIAL PRIMARY KEY,
     id_product INTEGER NOT NULL REFERENCES product(id_product),
@@ -72,6 +65,7 @@ CREATE TABLE storehouse (
     id_supply INTEGER NOT NULL REFERENCES supply(id_supply),
     received_at TIMESTAMP NOT NULL,
     expires_at TIMESTAMP,
-    status store_status_enum NOT NULL DEFAULT 'OK',
+    status VARCHAR(20) NOT NULL DEFAULT 'OK'
+        CHECK (status IN ('OK', 'NEAR_EXPIRY', 'SPOILED')),
     id_reception INTEGER REFERENCES reception(id_reception)
 );
